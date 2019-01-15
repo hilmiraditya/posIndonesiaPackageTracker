55277cd82cd70c22b48d7a0145ccb07659823f78
Ismail Syarief <ismail8syarief@gmail.com>
2019-01-15 11:55:37 +0700

    splashscreen

---
+++ app/src/main/java/com/example/coba/posindonesia/SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package com.example.coba.posindonesia;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Fade;
+import android.util.Pair;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Fade fade = new Fade();
+        View decor = getWindow().getDecorView();
+        fade.excludeTarget(decor.findViewById(R.id.action_bar_container),true);
+        fade.excludeTarget(android.R.id.statusBarBackground,true);
+        fade.excludeTarget(android.R.id.navigationBarBackground,true);
+
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
+
+        getWindow().setAllowReturnTransitionOverlap(false);
+
+        final View vAppLogo = this.findViewById(R.id.imageView2);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Pair<View, String> p1 = Pair.create(vAppLogo, "AppLogo");
+                Intent goToMain = new Intent(SplashScreenActivity.this, MainActivity.class);
+                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashScreenActivity.this, p1);
+                startActivity(goToMain, activityOptions.toBundle());
+
+                finish();
+            }
+        }, 2000L);
+    }
+}
