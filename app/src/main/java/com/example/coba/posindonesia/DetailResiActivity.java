a4e867b972a20a45be7c221a4c9976f1e976e1e5
Hilmi Raditya <raditya113@icloud.com>
2019-01-18 15:14:02 +0700

    mapsnya aneh

--- app/src/main/java/com/example/coba/posindonesia/DetailResiActivity.java
+++ app/src/main/java/com/example/coba/posindonesia/DetailResiActivity.java
@@ -1,7 +1,12 @@
 package com.example.coba.posindonesia;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,11 +16,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.directions.route.Routing;
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.here.android.mpa.common.PositioningManager;
+import com.here.android.mpa.mapping.MapCircle;
 import com.here.android.mpa.mapping.MapFragment;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.mapping.Map;
@@ -25,128 +37,120 @@ import com.google.android.gms.maps.model.LatLng;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapMarker;
 import com.here.android.mpa.mapping.MapObject;
 import com.here.android.mpa.mapping.OnMapRenderListener;
 import com.here.android.mpa.routing.RouteManager;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.zip.Inflater;
 
-public class DetailResiActivity extends AppCompatActivity{
+public class DetailResiActivity extends AppCompatActivity {
 
 //    private RecyclerView recyclerView;
 //    private RecyclerView.Adapter adapter;
 //    private RecyclerView.LayoutManager layoutManager;
 //    private ArrayList<String> dataSet;
 
-//    @Override
-//    public void onMapReady(Map map) {
-//        map.setCenter(new GeoCoordinate(37.7397, -121.4252, 0.0), Map.Animation.NONE);
-//        map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
-//    }
-
     private Map map = null;
     private MapFragment mapFragment = null;
-    private MapObject mapObject;
+    private static final int REQUEST_CODE_PERMISSION = 1;
+    String mPermission = Manifest.permission.ACCESS_FINE_LOCATION;
+    Double latitude, longitude;
+    MapCircle mapCircle;
+    View v;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private void permission(){
+        if(Build.VERSION.SDK_INT>= 23) {
+            if (checkSelfPermission(mPermission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(DetailResiActivity.this,
+                        new String[]{mPermission,
+                        }, REQUEST_CODE_PERMISSION);
+                return;
+            } else {
+                FusedLocationProviderClient mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
+                mFusedLocation.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null){
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            Log.i("BERHASIL LAT",String.valueOf(latitude));
+                            Log.i("BERHASIL LONG",String.valueOf(longitude));
+                        }
+                    }
+                });
+            }
+        }
+    }
 
-
-
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_resi);
-        final OnMapReadyCallback onMapReadyCallback = null;
-        TextView noDetailResi = this.findViewById(R.id.noDetailResi);
-        Log.i("SRRRR", getIntent().getStringExtra("NoResi"));
-        noDetailResi.setText(getIntent().getStringExtra("NoResi"));
-
-        Fade fade = new Fade();
-        View decor = getWindow().getDecorView();
-        fade.excludeTarget(decor.findViewById(R.id.action_bar_container), true);
-        fade.excludeTarget(android.R.id.statusBarBackground, true);
-        fade.excludeTarget(android.R.id.navigationBarBackground, true);
-
-        getWindow().setEnterTransition(fade);
-        getWindow().setExitTransition(fade);
-
-        getWindow().setAllowReturnTransitionOverlap(false);
-
-//        detailResiBtn.findViewById(R.id.detailResiBtn);
-//        detailResiBtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                bottom = new DetailBottomSheet();
-//                bottom.show(getSupportFragmentManager(), bottom.getTag());
-//            }
-//        });
-
+    private void maps(){
+        mapCircle = new MapCircle(200.5 ,new GeoCoordinate(-6.899042700000001,107.6185359));
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
         mapFragment.init(new OnEngineInitListener() {
             @Override
             public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
                 if (error == OnEngineInitListener.Error.NONE) {
                     map = mapFragment.getMap();
-                    map.setCenter(new GeoCoordinate(-6.874230, 107.616400), Map.Animation.NONE);
+                    map.addMapObject(mapCircle);
                     map.setZoomLevel(map.getMaxZoomLevel());
-                    
-
+                    map.getCenter();
                 }else{
                     Log.i("HERE ERR", error.getDetails());
                 }
             }
         });
+    }
 
+    private void refresh_maps(){
+        v.setVisibility(View.GONE);
+
+
+    }
+
+    private void load_maps_view(){
+        ViewStub stub = (ViewStub) findViewById(R.id.MAP_ETA);
+        stub.setLayoutResource(R.layout.maps_eta);
+        v = stub.inflate();
+        maps();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_resi);
+        load_maps_view();
+        final OnMapReadyCallback onMapReadyCallback = null;
+        TextView noDetailResi = this.findViewById(R.id.noDetailResi);
+        Log.i("SRRRR", getIntent().getStringExtra("NoResi"));
+        noDetailResi.setText(getIntent().getStringExtra("NoResi"));
+        Fade fade = new Fade();
+        View decor = getWindow().getDecorView();
+        fade.excludeTarget(decor.findViewById(R.id.action_bar_container), true);
+        fade.excludeTarget(android.R.id.statusBarBackground, true);
+        fade.excludeTarget(android.R.id.navigationBarBackground, true);
+        getWindow().setEnterTransition(fade);
+        getWindow().setExitTransition(fade);
+        getWindow().setAllowReturnTransitionOverlap(false);
         Button button = (Button) findViewById(R.id.detailResiBtn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 BottomSheetDialogFragment bottom = new DetailBottomSheet();
-                bottom.show(getSupportFragmentManager(),bottom.getTag());
+                bottom.show(getSupportFragmentManager(), bottom.getTag());
             }
         });
 
-//        FloatingActionButton refresh_button = (FloatingActionButton) findViewById(R.id.refresh_button);
-//        refresh_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //do here
-//            }
-//        });
-
+        FloatingActionButton refresh_button = (FloatingActionButton) findViewById(R.id.refresh_button);
+        refresh_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refresh_maps();
+            }
+        });
+        permission();
     }
-
-
-
-
-
-
-//        ///adapter///
-//        dataSet = new ArrayList<>();
-//        setDataSet();
-//        recyclerView = new RecyclerView(DetailResiActivity.this);
-//        recyclerView.findViewById(R.id.detil_recycleview);
-//        recyclerView.setHasFixedSize(true);
-//
-//        layoutManager = new LinearLayoutManager(this);
-//        recyclerView.setLayoutManager(layoutManager);
-//
-//        adapter = new RecyclerViewAdapter(dataSet);
-//        recyclerView.setAdapter(adapter);
-//        ////
-    }
-
-//    private void setDataSet(){
-//        dataSet.add("HALO SATU DUA TIGA");
-//        dataSet.add("hehehe");
-//        dataSet.add("hehehe");
-//        dataSet.add("hehehe");
-//        dataSet.add("hehehe");
-//        dataSet.add("hehehe");
-//        dataSet.add("hehehe");
-//        dataSet.add("hehehe");
-//        dataSet.add("hehehe");
-//    }
-
+}
 
