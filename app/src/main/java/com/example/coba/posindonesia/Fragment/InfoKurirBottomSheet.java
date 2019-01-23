49fc702cce9be64aed865c8458a99c9bfd06af84
Hilmi Raditya <raditya113@icloud.com>
2019-01-23 14:35:10 +0700

    sms kurir

--- app/src/main/java/com/example/coba/posindonesia/Fragment/InfoKurirBottomSheet.java
+++ app/src/main/java/com/example/coba/posindonesia/Fragment/InfoKurirBottomSheet.java
@@ -1,19 +1,27 @@
 package com.example.coba.posindonesia.Fragment;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.coba.posindonesia.Activity.MainActivity;
 import com.example.coba.posindonesia.R;
 
 public class InfoKurirBottomSheet extends BottomSheetDialogFragment {
 
     Button call,sms;
+    String phoneNo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -33,11 +41,11 @@ public class InfoKurirBottomSheet extends BottomSheetDialogFragment {
         sms.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri smsUri = Uri.parse("tel:087855857881");
-                Intent intent = new Intent(Intent.ACTION_VIEW, smsUri);
-                intent.putExtra("sms_body", "shenrenkui");
-                intent.setType("vnd.android-dir/mms-sms");
-                startActivity(intent);
+                phoneNo = "087855857881";
+                Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+                smsIntent.setData(Uri.parse("smsto:"));
+                smsIntent.putExtra("address"  , phoneNo);
+                startActivity(smsIntent);
             }
         });
         return view;
