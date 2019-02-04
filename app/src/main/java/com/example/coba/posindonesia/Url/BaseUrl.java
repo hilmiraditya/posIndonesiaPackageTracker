e2dcc41b7b00d0801d811e31a9f13c75d7144b1a
Hilmi Raditya <raditya113@icloud.com>
2019-02-04 22:36:15 +0700

    all done

--- app/src/main/java/com/example/coba/posindonesia/Url/BaseUrl.java
+++ app/src/main/java/com/example/coba/posindonesia/Url/BaseUrl.java
@@ -4,7 +4,9 @@ public class BaseUrl {
 
 //    private String Url = "http://10.1.9.94:1001/api/";
 //    private String Url = "http://10.1.9.240:8000/api/";
+    //beres
     private String Url = "http://192.168.1.195:8000/api/";
+
     public String getUrl(){
         return this.Url;
     }
