f456ffefa454d9683f22e4ac59ee68843ed84a1e
Hilmi Raditya <raditya113@icloud.com>
2019-02-04 22:30:12 +0700

    nyiapin merge

--- app/src/main/java/com/example/coba/posindonesia/Url/BaseUrl.java
+++ app/src/main/java/com/example/coba/posindonesia/Url/BaseUrl.java
@@ -3,7 +3,8 @@ package com.example.coba.posindonesia.Url;
 public class BaseUrl {
 
 //    private String Url = "http://10.1.9.94:1001/api/";
-    private String Url = "http://10.1.9.240:8000/api/";
+//    private String Url = "http://10.1.9.240:8000/api/";
+    private String Url = "http://192.168.1.195:8000/api/";
     public String getUrl(){
         return this.Url;
     }
